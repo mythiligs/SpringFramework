725857015de93228780e2f56b9fadf6b08be53e0
mythiligs <mythiligs27@gmail.com>
2025-03-14 13:13:45 +0530

    day4,restapi jpa

---
+++ SpringBootRestApIJPA/src/main/java/com/test/dao/CustomerDao.java
@@ -0,0 +1,12 @@
+package com.test.dao;
+
+import java.util.List;
+
+import com.test.entity.Customer;
+
+public interface CustomerDao {
+	public Customer createCustomer(Customer cst);
+	public List<Customer>readAllCustomer();
+	public List<Customer>updateCustomer(Customer cst);
+	public List<Customer>deleteCustomer(int id);
+}
