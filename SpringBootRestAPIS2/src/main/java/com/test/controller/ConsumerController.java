7d4307058f4b4748bdf8a673af0d0f227d705ef3
mythiligs <mythiligs27@gmail.com>
2025-03-19 09:13:55 +0530

    day7,cloudconfig,p3

---
+++ SpringBootRestAPIS2/src/main/java/com/test/controller/ConsumerController.java
@@ -0,0 +1,15 @@
+package com.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/consumer")
+public class ConsumerController {
+	@GetMapping("/service2")
+	public String consumerService()
+	{
+		return "Consumer Controller Response";
+	}
+}
